915ea7aefe920c39c9ab60af235c29d0cf5eb4c4
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-03-21 00:54:23 +0600

    implemented social social

--- server/api.py
+++ server/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import credentials, firestore, initialize_app, auth
 from pydantic import BaseModel
 from typing import Optional
 import os
@@ -11,45 +11,38 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Initialize Firestore
 cred = credentials.Certificate(os.path.join(BASE_DIR,"service-account.json"))
-initialize_app(cred)
+firebase = initialize_app(cred)
 db = firestore.client()
 
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-@app.post("/login/social/{provider}")
+@app.get("/login/social/{provider}")
 async def social_login(provider:str, request:Request):
     # parse token from request header
-    token = request.headers.get('Authorization').replace("Bearer ", "")
+    token = request.headers.get('Authorization').split('Bearer ')[1]
     # validate token with social provider
+    try:
+        decoded_token = auth.verify_id_token(token)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    # exchange token with auth backend
-    # return access token from auth backend
+    # return  decoded_token(user info)
+    return decoded_token
 
 
-    # # Assuming the collection name is "users"
-    # users_ref = db.collection("users")
-    # user_doc = users_ref.where("provider", "==", provider).where("token", "==", token).limit(1).get()
-
-    # if not user_doc:
-    #     raise HTTPException(status_code=401, detail="Invalid credentials")
-
-    # user_data = user_doc[0].to_dict()
-
-    # return user_data
-
-    return {'provider': provider, 'token': token}
-
-# Dummy endpoint for demonstration purposes
 @app.get("/users/me")
-async def read_current_user(token: str = Depends(oauth2_scheme)):
-    # Assuming the collection name is "users"
-    users_ref = db.collection("users")
-    user_doc = users_ref.where("token", "==", token).limit(1).get()
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        token = token.split('Bearer ')[1]
+        decoded_token = auth.verify_id_token(token)
+        uid = decoded_token['uid']
+    except:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    # get user data from firestore
+    user = db.collection('users').document(uid).get()
+    return user.to_dict()
 
-    if not user_doc:
-        raise HTTPException(status_code=401, detail="Unauthorized")
 
-    user_data = user_doc[0].to_dict()
-    return user_data
