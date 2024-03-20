9452c06f9757b3b267efd70b95accbc622aa033a
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-03-20 19:07:39 +0600

    initial

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-# FastAPI on Vercel Starter
+# Auth Backend for Project Dokan
 
 Built with FastAPI
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Ffardeenes7%2Ffastapi-starter&project-name=fastapi&repository-name=fastapi&demo-title=FastAPI%20Starter)
--- server/api.py
+++ server/api.py
@@ -1,11 +1,55 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.security import OAuth2PasswordBearer
+from firebase_admin import credentials, firestore, initialize_app
+from pydantic import BaseModel
+from typing import Optional
+import os
+import requests
 app = FastAPI()
 
-@app.get("/", tags=["Root"])
-async def read_root() -> dict:
-    return {
-        "message": "Hello World"
-    }
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# app.include_router(NoteRouter, prefix="/note")
+# Initialize Firestore
+cred = credentials.Certificate(os.path.join(BASE_DIR,"service-account.json"))
+initialize_app(cred)
+db = firestore.client()
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+@app.post("/login/social/{provider}")
+async def social_login(provider:str, request:Request):
+    # parse token from request header
+    token = request.headers.get('Authorization').replace("Bearer ", "")
+    # validate token with social provider
+
+    # exchange token with auth backend
+    # return access token from auth backend
+
+
+    # # Assuming the collection name is "users"
+    # users_ref = db.collection("users")
+    # user_doc = users_ref.where("provider", "==", provider).where("token", "==", token).limit(1).get()
+
+    # if not user_doc:
+    #     raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    # user_data = user_doc[0].to_dict()
+
+    # return user_data
+
+    return {'provider': provider, 'token': token}
+
+# Dummy endpoint for demonstration purposes
+@app.get("/users/me")
+async def read_current_user(token: str = Depends(oauth2_scheme)):
+    # Assuming the collection name is "users"
+    users_ref = db.collection("users")
+    user_doc = users_ref.where("token", "==", token).limit(1).get()
+
+    if not user_doc:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    user_data = user_doc[0].to_dict()
+    return user_data
